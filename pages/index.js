1d69654014c6915abeb40058206e87dea741e0eb
TonNomGitHub <excellentservice1exls@gmail.com>
2026-04-05 14:53:49 +0100

    mise à jour de index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 
-const Index = () => {
-  return (
+const Index = () => (
     <div>
       <h1>Welcome to the Boutique</h1>
       <p>Discover our latest collection!</p>
     </div>
-  )
-};
+)
 
 export default Index
