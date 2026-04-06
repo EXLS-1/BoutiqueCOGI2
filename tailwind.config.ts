7b66b9bdc028c5017ca73b36a6014b13b2833e75
TonNomGitHub <excellentservice1exls@gmail.com>
2026-04-06 22:14:52 +0100

    mise à jour

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,13 +13,13 @@ const Config: Config = {
     extend: {
       colors: {
         brand: {
-          turquoise: "40E0D0"
-          rose: "F43F5E"
+          turquoise: "rgb(64, 224, 208)"
+          rose: "rgb(244, 63, 94)"
         },
         secondary: {
-          red:
-          blue:
-          green:
+          red:"rgb(255,0,0)"
+          blue:"rgb(0,0,255)"
+          green:"rgb(0,255,0)"
         },
         // ...(Garde les variables par defaut de shadcn ici)
       },
